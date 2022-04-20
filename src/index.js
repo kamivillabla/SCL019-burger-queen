572e8cdd7b7decbea3842fd839f05d7dc3e434a6
Kamila <kvillablancabaez@gmail.com>
2022-04-19 22:14:09 -0400

    enrutamiento inicio y 404

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 /* import Menu from './pages/Menu'; */
 import Inicio from './pages/Inicio';
+import Menu from './pages/Menu'
+import Error404 from './pages/Error404'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
  {/*    <Menu /> */}
-    <Inicio />
-  </React.StrictMode>
+  {/*   <Inicio /> */}
+    <Routes>
+      <Route path="/" element={<Inicio />} />
+      <Route path="Menu" element={<Menu />} />
+      <Route path="*" element={<Error404 />} />
+    </Routes>
+  </BrowserRouter>
 );
 
--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../css/page-inicio.css'
 import '../App.css'
+import { Link } from 'react-router-dom'
 
 const inicio = () => {
     return (
@@ -9,10 +10,11 @@ const inicio = () => {
                 <img className='img-logo' src={require('../img/pizza-otrologo.png')} alt="Imagen pizza Logo" />
             </div>
             <div className="contenedor d-flex justify-content-center ">
-                <button className='button-inicio'>Mesas</button>
+                <Link className='button-inicio' to="/Menu">Mesas</Link>
                 <button className='button-inicio'>Cocina</button>
                 <button className='button-inicio'>Pedidos listos</button>
             </div>
+            
         </div>
     )
 }
