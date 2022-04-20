d719991c3522ee252aad6f7b3960a62ceded39a8
Kamila <kvillablancabaez@gmail.com>
2022-04-19 23:35:25 -0400

    enrutamiento de menu a inicio

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,18 +2,23 @@ import React from 'react'
 import '../App.css';
 import '../css/page-menu.css'
 import Tabmenu from '../components/Tabmenu';
+import NavBar from '../components/Nav';
 
 const Menu = () => {
   return (
-    <div className='container-fluid d-flex flex-column justify-content-center container-menu'>
-      <div className="container row">
-        {/* Menú */}
-        <div className="col-12 col-md-6">
-          <Tabmenu />
-        </div>
-        {/* Pedido total */}
-        <div className="col-12 col-md-6">
-
+    <div className='container-menu--color'>
+      <NavBar />
+      <div className='container-menu'>
+        <div className='container-fluid d-flex flex-column justify-content-center '>
+          <div className="container row">
+            {/* Menú */}
+            <div className="col-12 col-md-6">
+              <Tabmenu />
+            </div>
+            {/* Pedido total */}
+            <div className="col-12 col-md-6">
+            </div>
+          </div>
         </div>
       </div>
     </div>
