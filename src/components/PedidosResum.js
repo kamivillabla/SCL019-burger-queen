4ae13388dd4e1d4a0ef98560e36fdd2659c5a3ab
Kamila <kvillablancabaez@gmail.com>
2022-04-20 20:40:59 -0400

    useState en componente de resumen

---
+++ src/components/PedidosResum.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../css/resumen.css";
+import "../App.css";
+
+const PedidosResum = () => {
+  return (
+    <>
+      <div className="d-flex justify-content-around align-items-center mt-5 container-pedido">
+        <span className="circle"></span>
+        <h3>Pizza Vegetariana</h3>
+        <h4>$3999</h4>
+        <i class="fa-solid fa-trash-arrow-up resumen-icons"></i>
+      </div>
+      <hr className="resumen-border mt-2" />
+    </>
+  );
+};
+
+export default PedidosResum;
--- src/components/Resumen.js
+++ src/components/Resumen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AnotacionesResum from "./AnotacionesResum";
+import PedidosResum from "./PedidosResum";
 import "../App.css";
 import "../css/resumen.css";
 
@@ -9,7 +10,7 @@ const Resumen = () => {
       <div className="container resumen-container mt-5">
         <div className="d-flex resumen-cliente mt-4">
           <h3>Cliente:</h3>
-          <p>Karla Jara</p>
+          <p>karla</p>
         </div>
         <div className="d-flex resumen-mesa">
           <h3>Mesa:</h3>
@@ -17,25 +18,9 @@ const Resumen = () => {
         </div>
         <hr className="resumen-border" />
         {/* Iterable */}
-        <div className="d-flex justify-content-around align-items-center mt-5 container-pedido">
-          <span className="circle"></span>
-          <h3>Pizza Vegetariana</h3>
-          <h4>$3999</h4>
-          <i class="fa-solid fa-trash-arrow-up resumen-icons"></i>
-        </div>
-        <div className="d-flex justify-content-around align-items-center mt-5 container-pedido">
-          <span className="circle"></span>
-          <h3>Pizza Vegetariana</h3>
-          <h4>$3999</h4>
-          <i class="fa-solid fa-trash-arrow-up resumen-icons"></i>
-        </div>
-        <div className="d-flex justify-content-around align-items-center mt-5 container-pedido">
-          <span className="circle"></span>
-          <h3>Pizza Vegetariana</h3>
-          <h4>$3999</h4>
-          <i class="fa-solid fa-trash-arrow-up resumen-icons"></i>
-        </div>
-        <hr className="resumen-border mt-2" />
+        <PedidosResum />
+        <PedidosResum />
+        <PedidosResum />
         {/* Total */}
         <div className="d-flex justify-content-end">
           <p>Total: $23500</p>
